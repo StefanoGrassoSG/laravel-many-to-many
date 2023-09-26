2e4c394ee1d007c948020dad8a4efce254d2b4ac
StefanoGrassoSG <stefanograssosg92@gmail.com>
2023-09-26 16:28:08 +0200

    ADD dynamic data in dashboard

--- app/Http/Controllers/Admin/PrivateController.php
+++ app/Http/Controllers/Admin/PrivateController.php
@@ -5,10 +5,19 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+//models
+use App\Models\Project;
+use App\Models\Type;
+use App\Models\Technology;
+
 class PrivateController extends Controller
 {
     public function dashboard() {
 
-        return view('admin.dashboard');
+       $projects = Project::count();
+       $types = Type::count();
+       $technologies = Technology::count();
+
+        return view('admin.dashboard', compact('projects', 'types', 'technologies'));
     }
 }
--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,14 +4,56 @@
 
 @section('main-content')
     <div class="row">
-        <div class="col">
+        <div class="col-12 col-sm-6 col-md-4">
             <div class="card">
                 <div class="card-body">
-                    <h1 class="text-center text-success">
-                        Sei loggato!
-                    </h1>
-                    <br>
-                    La dashboard è una pagina privata (protetta dal middleware)
+                    <h2 class="text-center text-success fs-4">
+                        Published Projects
+                    </h2>
+                    <h3 class="text-center fs-2">
+                        {{ $projects }}
+                    </h3>
+                    <div class="text-center mt-4">
+                        <a href="{{ route('admin.projects.index') }}" class="btn btn-primary">
+                            See All
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-6 col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h2 class="text-center text-success fs-4">
+                        Types
+                    </h2>
+                    <h3 class="text-center fs-2">
+                        {{ $types }}
+                    </h3>
+                    <div class="text-center mt-4">
+                        <a href="{{ route('admin.types.index') }}" class="btn btn-primary">
+                            See All
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-12 col-sm-6 col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <h2 class="text-center text-success fs-4">
+                        Technologies
+                    </h2>
+                    <h3 class="text-center fs-2">
+                        {{ $technologies }}
+                    </h3>
+                    <div class="text-center mt-4">
+                        <a href="{{ route('admin.technologies.index') }}" class="btn btn-primary">
+                            See All
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
